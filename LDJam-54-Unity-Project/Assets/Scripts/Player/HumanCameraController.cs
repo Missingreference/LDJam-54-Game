5b1904dcec0796aa2ce354510166b2db391f84a1
Missingreference <Missingreference@users.noreply.github.com>
2023-09-30 10:48:03 -0400

    Implemented Simple Camera Smoothing

--- LDJam-54-Unity-Project/Assets/Scripts/Player/HumanCameraController.cs
+++ LDJam-54-Unity-Project/Assets/Scripts/Player/HumanCameraController.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 public class HumanCameraController : MonoBehaviour
 {
     public Human target;
+    public float lookaheadDistance = 1.0f;
+    public float dampening = 0.15f;
+    public float noDirectionDampening = 0.5f;
+
+    private Vector2 m_TargetPosition;
 
     void Awake()
     {
         if(target == null)
             target = FindObjectOfType<Human>();
+
     }
 
     void Start()
     {
-        
+
     }
 
-    void Update()
+    private Vector2 m_Velocity = Vector3.zero;
+
+    void LateUpdate()
     {
         if(target == null) return;
 
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10.0f);
+
+        float damp;
+
+        if(target.moveDirection != Vector2.zero)
+        {
+            damp = dampening;
+            Vector2 direction = target.moveDirection.normalized;
+            m_TargetPosition = target.transform.position + new Vector3(lookaheadDistance * direction.x, lookaheadDistance * direction.y, 0);
+        }
+        else
+        {
+            damp = noDirectionDampening;
+            m_TargetPosition = target.transform.position;
+        }
+
+        Vector2 newPosition = Vector2.SmoothDamp(transform.position, m_TargetPosition, ref m_Velocity, damp);
+        transform.position = new Vector3(newPosition.x, newPosition.y, -10.0f);
     }
 }
